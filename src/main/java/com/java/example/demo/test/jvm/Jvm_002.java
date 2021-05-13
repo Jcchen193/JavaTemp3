b6c92cb23ba81779506188d70b84673c46006818
jiechen <jiechen@ncsi.com.cn>
2021-05-13 14:46:19 +0800

    //add demo in test

---
+++ src/main/java/com/java/example/demo/test/jvm/Jvm_002.java
@@ -0,0 +1,9 @@
+package com.java.example.demo.test.jvm;
+
+public class Jvm_002 {
+	//Heap and Stack param
+	 public static void main(String[] args) {  
+		 System.out.println("Heap and Stack!");  
+	 }  
+
+}
