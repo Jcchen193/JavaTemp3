73b6b2c9e79d8b1ada48b33c4eae2ca5ca6c4a6a
P1312749 <xing.cao@ncsi.com.cn>
2021-06-08 18:18:34 +0800

    Update Cao Xing

--- src/main/java/com/java/example/demo/test/netty/chatgroup/GroupChatServerHandler.java
+++ src/main/java/com/java/example/demo/test/netty/chatgroup/GroupChatServerHandler.java
@@ -76,13 +76,21 @@ public class GroupChatServerHandler extends SimpleChannelInboundHandler<String> 
         Channel channel = ctx.channel();
         //这时我们遍历channelGroup, 根据不同的情况，回送不同的消息
 
+        for(Channel ch : channelGroup) {
+        	 if(channel != ch) { //不是当前的channel,转发消息
+                 ch.writeAndFlush("[客户]" + channel.remoteAddress() + " 发送了消息" + msg + "\n");
+             }else {//回显自己发送的消息给自己
+                 ch.writeAndFlush("[自己]发送了消息" + msg + "\n");
+             }
+		}
+        /*
         channelGroup.forEach(ch-> {
             if(channel != ch) { //不是当前的channel,转发消息
                 ch.writeAndFlush("[客户]" + channel.remoteAddress() + " 发送了消息" + msg + "\n");
             }else {//回显自己发送的消息给自己
                 ch.writeAndFlush("[自己]发送了消息" + msg + "\n");
             }
-        });
+        });*/
     }
 
     @Override
